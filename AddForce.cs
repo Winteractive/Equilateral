f9192f70c19c7e667ed65ef6e50ff03d16e2293c
Winteractive <winteractiveSE@gmail.com>
2016-11-23 22:43:38 +0100

    Add Force test + Off limit area

---
+++ AddForce.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddForce : MonoBehaviour
+{
+    public float force;
+    public float extraGrav;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<JelloSpringBody>().AddForce(Vector2.up * force);
+
+        }
+    }
+}
